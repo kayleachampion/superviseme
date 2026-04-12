9fce466ca88f6cc714ba095f17dd7b8dd104e3be
Kaylea Champion <khascall@gmail.com>
2026-04-11 20:39:55 -0700

    Refactor course IDs for consistency

--- supervisor-optimizer/src/App.jsx
+++ supervisor-optimizer/src/App.jsx
@@ -3,16 +3,16 @@ import React, { useState, useMemo } from "react";
 const COURSES = [
   { id: "CSS199", label: "CSS199", ppc: 1 / 50 },
   { id: "CSS499", label: "CSS499", ppc: 1 / 50 },
-  { id: "CSS497UGR", label: "CSS497UGR", ppc: 1 / 50 },
+  { id: "497UGR", label: "CSS497UGR", ppc: 1 / 50 },
   { id: "CSS198", label: "CSS198", ppc: 1 / 100 },
   { id: "CSS498", label: "CSS498", ppc: 1 / 100 },
-  { id: "CSS497PRJ", label: "CSS497PRJ", ppc: 1 / 100 },
+  { id: "497PRJ", label: "CSS497PRJ", ppc: 1 / 100 },
   { id: "CSS397", label: "CSS397", ppc: 1 / 150 },
   { id: "CSS495", label: "CSS495", ppc: 1 / 150 },
-  { id: "CSS497INT", label: "CSS497INT", ppc: 1 / 150 },
+  { id: "497INT", label: "CSS497INT", ppc: 1 / 150 },
   { id: "CSS595", label: "CSS595", ppc: 1/50},
-  { id: "CSS595", label: "CSS601", ppc: 1/150},
-  { id: "CSS595", label: "CSS600", ppc: 1/100},
+  { id: "CSS601", label: "CSS601", ppc: 1/150},
+  { id: "CSS600", label: "CSS600", ppc: 1/100},
   { id: "CSS700", label: "CSS700", ppc: 1/50}
 ];
 
